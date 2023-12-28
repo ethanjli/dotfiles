a4ad540aa4444ecc06ab46047a15a6d9adcf38a5
Ethan Li <lietk12@gmail.com>
2023-12-27 19:23:50 -0800

    Try to fix issues with resolving neovim in nested shells on musl hosts

--- dot_config/zsh/integrations/cli/nvim.sh
+++ dot_config/zsh/integrations/cli/nvim.sh
@@ -1,21 +1,11 @@
-# Note: this depends on an nvim_host_path env var to be set
-
-_zsh_integrations_nvim() {
-  unset -f "$0"
-  if command -v "$nvim_host_path" &> /dev/null; then # nvim is provided by the host, so just use that
-    local nvim_path="$nvim_host_path"
-    alias nvim="$nvim_path" # we must use an alias, not a function, to properly capture the path everywhere
-  elif command -v nvim &> /dev/null; then # fall back to nvim provided by aqua, which is broken on musl hosts
-    local nvim_path="$(which nvim)"
-  fi
-
-  if command -v "$nvim_path" &> /dev/null; then # nvim is provided by any source
-    export EDITOR="$nvim_path"
-    export VISUAL="$EDITOR"
-    alias vim=nvim
-  elif command -v vim &> /dev/null; then # try to fall back to vim
-    export EDITOR="$(which vim)"
-    export VISUAL="$EDITOR"
-  fi
-}
-_zsh_integrations_nvim
+if command -v nvim &> /dev/null; then # nvim is provided by the host, so just use that
+  export EDITOR="$(which nvim)"
+  alias vim=nvim
+elif command -v neovim &> /dev/null; then # try to fall back to neovim provided by aqua, which is broken on musl hosts
+  export EDITOR="$(which neovim)"
+  alias nvim=neovim
+  alias vim=neovim
+elif command -v vim &> /dev/null; then # try to fall back to vim
+  export EDITOR="$(which vim)"
+fi
+export VISUAL="$EDITOR"
