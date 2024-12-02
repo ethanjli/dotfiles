b76aba3fcf11616e3044d72578b208f3bad92c2e
Ethan Li <lietk12@gmail.com>
2024-12-01 22:46:11 -0800

    Work around WezTerm Flatpak problems for distrobox

--- dot_config/bash/integrations/20-wezterm-flatpak-wayland.sh
+++
@@ -1,6 +0,0 @@
-if [[ ! -z "$WAYLAND_DISPLAY" ]]; then
-  display="$(basename "$WAYLAND_DISPLAY")"
-  if [[ -S "${XDG_RUNTIME_DIR}/$display" ]]; then
-    export WAYLAND_DISPLAY="$display"
-  fi
-fi
--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -127,7 +127,10 @@ config.keys = {
 }
 
 -- set the default shell to zsh
-config.default_prog = { '/usr/bin/zsh', '-l' }
+-- note: we use dbus-launch as a workaround for an issue in the WezTerm flatpak where various env
+-- vars (e.g. $WAYLAND_DISPLAY and $DBUS_SESSION_BUS_ADDRESS) are set to nonexistent paths in
+-- `/run/flatpak` rather than `/run/user/1000`:
+config.default_prog = { '/usr/bin/dbus-launch', '/usr/bin/zsh', '-l' }
 config.set_environment_variables = {
   SHELL = '/usr/bin/zsh'
 }
