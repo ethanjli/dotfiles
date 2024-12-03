1f65d8d9637515ba66b827683f406d2b6b474f05
Ethan Li <lietk12@gmail.com>
2024-12-02 16:58:20 -0800

    Allow using lf within nvim to browse/open files

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -18,4 +18,16 @@ return {
       },
     },
   },
+  {
+    "rolv-apneseth/tfm.nvim",
+    opts = {
+      file_manager = "lf",
+      replace_netrw = true,
+      enable_cmds = true,
+      ui = {
+        height = 0.5,
+        width = 0.5,
+      },
+    },
+  },
 }
