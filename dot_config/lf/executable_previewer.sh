06403789d7b383c62a5bb413c150c81740ff7570
Ethan Li <lietk12@gmail.com>
2024-12-03 09:34:04 -0800

    Make sixel image preview in lf work within zellij

--- dot_config/lf/executable_previewer.sh
+++ dot_config/lf/executable_previewer.sh
@@ -13,64 +13,60 @@ file_path="$1"
 lowercased="$(echo "$file_path" | tr '[:upper:]' '[:lower:]')"
 mimetype="$(file -Lb --mime-type -- "$file_path")"
 
-
 preview_plain_text() {
   bat --color always --style plain --paging=never --tabs 2 \
     "$1"
 }
 
 preview_image() {
-  if [[ $ZELLIJ == "0" ]]; then
-    format="--format symbols" # In Zellij, the sixel format doesn't display for some reason
-  fi
-  chafa $format -s "$2x$3" --animate off --polite on "$1"
+  chafa -s "$2x$3" --animate off --polite on "$1"
 }
 
 case "$mimetype" in
-  image/*)
-    case "$mimetype" in
-      image/svg+xml)
-        thumbnail="$(get_tmpfile ".png")"
-        convert "$file_path" "$thumbnail"
-        preview_image "$thumbnail" "$2" "$3"
-        ;;
-      *)
-        preview_image "$file_path" "$2" "$3"
-        ;;
-    esac
-    exit 1
-    ;;
-  text/* | application/json | application/yaml)
-    case "$mimetype" in
-      text/plain)
-        case "$lowercased" in
-          #*.md)
-          #  glow -s dark "$1"
-          #  ;;
-          *)
-            preview_plain_text "$file_path"
-            ;;
-        esac
-        ;;
-      *)
-        preview_plain_text "$file_path"
-        ;;
-    esac
-    ;;
-  video/*)
+image/*)
+  case "$mimetype" in
+  image/svg+xml)
     thumbnail="$(get_tmpfile ".png")"
-    ffmpeg -y -i "$file_path" -vframes 1 "$thumbnail"
-    preview_image "$thumbnail" "$@"
+    convert "$file_path" "$thumbnail"
+    preview_image "$thumbnail" "$2" "$3"
     ;;
   *)
+    preview_image "$file_path" "$2" "$3"
+    ;;
+  esac
+  exit 1
+  ;;
+text/* | application/json | application/yaml)
+  case "$mimetype" in
+  text/plain)
     case "$lowercased" in
-      *.tar | *.zip | *.7z | *.gz | *.xz | *.lzma | *.bz | *.bz2 | *.lz4 | *.sz | *.zst | *.rar)
-        ouch list -t "$1"
-        ;;
-      *)
-        echo "Unknown file type: $mimetype"
-        preview_plain_text "$file_path"
-        ;;
+    #*.md)
+    #  glow -s dark "$1"
+    #  ;;
+    *)
+      preview_plain_text "$file_path"
+      ;;
     esac
     ;;
+  *)
+    preview_plain_text "$file_path"
+    ;;
+  esac
+  ;;
+video/*)
+  thumbnail="$(get_tmpfile ".png")"
+  ffmpeg -y -i "$file_path" -vframes 1 "$thumbnail"
+  preview_image "$thumbnail" "$@"
+  ;;
+*)
+  case "$lowercased" in
+  *.tar | *.zip | *.7z | *.gz | *.xz | *.lzma | *.bz | *.bz2 | *.lz4 | *.sz | *.zst | *.rar)
+    ouch list -t "$1"
+    ;;
+  *)
+    echo "Unknown file type: $mimetype"
+    preview_plain_text "$file_path"
+    ;;
+  esac
+  ;;
 esac
