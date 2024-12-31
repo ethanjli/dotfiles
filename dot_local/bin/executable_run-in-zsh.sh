d5e701e9076abf9259f6bb7d05a8ff1f00c15c35
Ethan Li <lietk12@gmail.com>
2024-12-31 14:13:00 -0800

    Fix issues with `.desktop` launchers of aqua-managed binaries

---
+++ dot_local/bin/executable_run-in-zsh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env -S zsh -i
+# This script runs the specified binary in an interactive zsh session (so that dotfiles are loaded),
+# forwarding all command-line arguments to the binary in an exact manner.
+
+$@
